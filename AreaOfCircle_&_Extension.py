c33a563c87bf0b69fffada7cee4ee12e6f9abc6c
Yatharth2774 <125198793+Yatharth2774@users.noreply.github.com>
2023-10-10 10:46:07 +0530

    AreaOfCircle_&_Extension.py
    Python program to calculate area of circle by entering its radius. It also includes program to check file type by entering file name along with its extension

---
+++ AreaOfCircle_&_Extension.py
@@ -0,0 +1,23 @@
+#write a Python program which accepts the radius of a circle from the user and computes area.
+import math
+radius = float(input("Enter  radius of circle : "))
+PI = math.pi
+
+area = PI*radius*radius
+
+print("Area of circle with radius : ",radius, " is : ", area)
+
+
+#write a Python program to accept a filename from the user and print the extension of that.
+filename = input("Enter complete filename : ")
+extension = (filename.split(".")[-1]).lower()
+if extension == 'c' :
+    print("The extension of the file is : C")
+elif extension == "py":
+    print("The extension of the file is : Python")
+elif extension == "cpp":
+    print("The extension of the file is : C++")
+elif extension == "java":
+    print("The extension of the file is : Java")
+else:
+    print("Extension Unknown!!!")
