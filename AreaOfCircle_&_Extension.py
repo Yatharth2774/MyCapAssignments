956dabe540b05353d3e542695cedfbdf07c612d0
Yatharth2774 <125198793+Yatharth2774@users.noreply.github.com>
2023-10-25 20:23:52 +0530

    AreaOfCircle_&_Extension.py

--- AreaOfCircle_&_Extension.py
+++ AreaOfCircle_&_Extension.py
@@ -10,7 +10,7 @@ print("Area of circle with radius : ",radius, " is : ", area)
 
 #write a Python program to accept a filename from the user and print the extension of that.
 filename = input("Enter complete filename : ")
-extension = (filename.split(".")[-1]).lower()
+extension = (filename.split(".")[-1]).lower()) #takes last element of the list i.e. the extension part onnly
 if extension == 'c' :
     print("The extension of the file is : C")
 elif extension == "py":
